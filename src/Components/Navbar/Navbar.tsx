471ff8ca7d4ec9a1d88ef40f594bf71f99180c69
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-10 13:58:34 +0300

    refactor(power implicity): clear files from unused code and imports

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import './Navbar.scss';
 
+export const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			<a className='navbar__logo'></a>
+			<ul className='navbar__links'>
+				<li className='navbar__link navbar__link--hover'></li>
+				<li className='navbar__link navbar__link--hover'></li>
+				<li className='navbar__link navbar__link--hover'></li>
+			</ul>
+			<button type='button' className='navbar__download-btn'></button>
+		</nav>
+	)
+}
