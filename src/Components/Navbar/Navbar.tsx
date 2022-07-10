785083f9f0e129bf55631c6be0a3b3b37a2e5d46
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-10 20:22:07 +0300

    feat(power implicity): add navbar screensize adaptivity

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from "./assets/logo.svg"
 const Navbar = () => {
 	return (
 		<nav className='navbar'>
-			<img src={logo} alt="navbar__logo" />
+			<img src={logo} className='navbar__logo' alt="navbar__logo" />
 			<ul className='navbar__links'>
 				<li className='navbar__link navbar__link--hover'>Features</li>
 				<li className='navbar__link navbar__link--hover'>Partners</li>
