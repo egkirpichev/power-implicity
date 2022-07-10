d54c0e37a952946686b77be28ea434dae5367e15
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-10 20:05:47 +0300

    feat(power implicity): add logo icon, refactor navbar and slider styles

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import "./Navbar.scss";
+import logo from "./assets/logo.svg"
 
 const Navbar = () => {
 	return (
 		<nav className='navbar'>
-			<img src="./logo.png" alt="navbar__logo" />
+			<img src={logo} alt="navbar__logo" />
 			<ul className='navbar__links'>
 				<li className='navbar__link navbar__link--hover'>Features</li>
 				<li className='navbar__link navbar__link--hover'>Partners</li>
