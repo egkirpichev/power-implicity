d07c9556b04ee23f99b94f56f99bc112c1d7859c
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-11 20:24:00 +0300

    refactor(power implicity): refactor navbar sryles and classnames, move assets folder to corect directory

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import "./Navbar.scss";
-import logo from "./assets/logo.svg"
+import logo from "../../assets/logo.svg"
 
 const Navbar = () => {
 	return (
 		<nav className='navbar'>
 			<img src={logo} className='navbar__logo' alt="navbar__logo" />
 			<ul className='navbar__links'>
-				<li className='navbar__link navbar__link--hover'>Features</li>
-				<li className='navbar__link navbar__link--hover'>Partners</li>
-				<li className='navbar__link navbar__link--hover'>Stories</li>
+				<li className='navbar__link'>Features</li>
+				<li className='navbar__link'>Partners</li>
+				<li className='navbar__link'>Stories</li>
 			</ul>
 			<button type='button' className='navbar__download-btn'>Download for free</button>
 		</nav>
