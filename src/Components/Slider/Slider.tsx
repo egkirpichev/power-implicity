59afab4354e5d3674785f03b0df370f3aca98d33
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-11 20:28:00 +0300

    refactor(power implicity): refactor slider component's sryles and classnames, move assets folder to corect directory

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -7,11 +7,11 @@ const Slider = () => {
 			<p className="slider__description">Instead of spending time searching for the right app, our AI will bring the right app to you.</p>
 			<button type="button" className="slider__button">Learn</button>
 			<ul className="slider__dots">
-				<li className="slider__dot Slider__dot--active"></li>
-				<li className="slider__dot Slider__dot--active"></li>
-				<li className="slider__dot Slider__dot--active"></li>
-				<li className="slider__dot Slider__dot--active"></li>
-				<li className="slider__dot Slider__dot--active"></li>
+				<li className="slider__dot"></li>
+				<li className="slider__dot"></li>
+				<li className="slider__dot"></li>
+				<li className="slider__dot"></li>
+				<li className="slider__dot"></li>
 			</ul>
 		</div>
 	)
