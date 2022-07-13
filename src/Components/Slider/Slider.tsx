68d271637b6c260d359786a45883742230af4633
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-07-13 21:28:16 +0300

    refactor(power implicity): remove pseudo class from slider dot element, add modifier class

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -9,7 +9,7 @@ const Slider = () => {
 			<ul className="slider__dots">
 				<li className="slider__dot"></li>
 				<li className="slider__dot"></li>
-				<li className="slider__dot"></li>
+				<li className="slider__dot slider__dot--active"></li>
 				<li className="slider__dot"></li>
 				<li className="slider__dot"></li>
 			</ul>
